ee93873cb3e7584f17cfdbcea6e3e18b1896b59a
rldnjs7723 <rldnjs7723@naver.com>
2023-04-20 20:29:35 +0900

    KMP 알고리즘 아호-코라식처럼 구현하는 방법 정리

--- BOJ/11000/Main_11585.java
+++ BOJ/11000/Main_11585.java
@@ -9,6 +9,7 @@ import java.util.StringTokenizer;
  * @author Giwon
  */
 public class Main_11585 {
+	public static final int MAX = 1000000;
 	public static final char ROOT = 0;
 
 	public static void main(String[] args) throws IOException {
@@ -26,21 +27,45 @@ public class Main_11585 {
 		for(int i = 0; i < N; i++) {
 			patternArr[i] = st.nextToken().charAt(0);
 		}
+		Pattern pattern = new Pattern(patternArr, N);
+		// Fail 함수 구성
+		pattern.findFail();
 		
 		// 문자열 구성 
 		StringBuilder sb = new StringBuilder();
+		st = new StringTokenizer(br.readLine());
 		// 동일한 문자열 2번 반복
-		for(int iter = 0; iter < 2; iter++) {
-			for(int i = 0; i < N; i++) {
-				sb.append(patternArr[i]);
-			}
+		for(int i = 0; i < N; i++) {
+			sb.append(st.nextToken().charAt(0));
 		}
+		sb.append(sb.toString());
 		String roullete = sb.toString();
 		
-		System.out.println(roullete);
+		// KMP로 패턴과 동일한 문자열 탐색
+		int count = 0;
+		Node curr = pattern.root;
+		// 문자열 로딩
+		for(int i = 0; i < N - 1; i++) {
+			curr = pattern.getNext(curr, roullete.charAt(i));
+		}
+		for(int i = N - 1; i < SIZE - 1; i++) {
+			curr = pattern.getNext(curr, roullete.charAt(i));
+			// 리프 노드라면 패턴과 일치하는 경우
+			if(curr.next == null) count++; 
+		}
 		
-		
+		int GCD = getGCD(count, N);
+		System.out.println((count / GCD) + "/" + (N / GCD));
 		br.close();
+	}
+	
+	// 최대 공약수 계산
+	public static int getGCD(int A, int B) {
+		for(int i = Math.min(MAX, A); i >= 1; i++) {
+			if(A % i == 0 && B % i == 0) return i;
+		}
+		
+		return 1;
 	}
 	
 	public static class Node {
@@ -62,6 +87,11 @@ public class Main_11585 {
 			this.next = new Node(val);
 			return this.next;
 		}
+
+		@Override
+		public String toString() {
+			return "Node [val=" + val + ", fail=" + fail.val + "]";
+		}
 	}
 	
 	// 패턴 문자열
@@ -78,9 +108,44 @@ public class Main_11585 {
 			for(int i = 0; i < N; i++) {
 				curr = curr.putNode(patternArr[i]);
 			}
-			
 		}
 		
+		// Fail 시 이동할 노드 탐색
+		public void findFail() {
+			// 루트 노드의 Fail은 루트 노드
+			root.fail = root;
+			
+			Node curr = root;
+			Node next, fail;
+			for(int i = 0; i < N; i++) {
+				next = curr.next;
+				
+				if(curr == root) {
+					// 루트 노드의 자식 노드의 Fail은 루트 노드
+					next.fail = root;
+				} else {
+					fail = curr.fail;
+					while(fail != root && fail.next.val != next.val) {
+						fail = fail.fail;
+					}
+					if(fail.next.val == next.val) fail = fail.next;
+					
+					next.fail = fail;
+				}
+				
+				curr = next;
+			}
+		}
+		
+		// 입력 받은 노드의 다음 노드 탐색
+		public Node getNext(Node curr, char val) {
+			while(curr != root && (curr.next == null || curr.next.val != val)) {
+				curr = curr.fail;
+			}
+			if(curr.next.val == val) curr = curr.next;
+			
+			return curr;
+		}
 		
 	}
 
--- Ideas/String.md
+++ Ideas/String.md
@@ -13,7 +13,7 @@
 
 ### 풀이 아이디어
 
-LCS 알고리즘이 기억나지 않아 다시 공부하고 풀었던 문제. [(정리)](https://github.com/rldnjs7723/CodingTest/#longest-common-subsequence-lcs-9252)
+[LCS 알고리즘](https://github.com/rldnjs7723/CodingTest/#longest-common-subsequence-lcs-9252)이 기억나지 않아 다시 공부하고 풀었던 문제.
 
 ## 2. BOJ_1786 찾기 [(백준 링크)](https://www.acmicpc.net/problem/1786) [(소스 코드)](https://github.com/rldnjs7723/CodingTest/blob/main/BOJ/1000/Main_1786.java)
 
@@ -25,3 +25,14 @@ LCS 알고리즘이 기억나지 않아 다시 공부하고 풀었던 문제. [(
 
 가장 기본적인 [KMP 알고리즘](https://github.com/rldnjs7723/CodingTest#knuth-morris-pratt-kmp-pattern-matching) 적용 문제.  
 구현이 어렵기 때문에 그냥 외우자.
+
+## 3. BOJ_11585 속타는 저녁 메뉴 [(백준 링크)](https://www.acmicpc.net/problem/11585) [(소스 코드)](https://github.com/rldnjs7723/CodingTest/blob/main/BOJ/11000/Main_11585.java)
+
+### 문제 요약
+
+문자열이 주어지고, 해당 문자열을 한 칸씩 Queue처럼 맨 앞에 위치한 문자를 맨 뒤로 보냈을 때 패턴 문자열이 등장하는 개수를 세는 문제
+
+### 풀이 아이디어
+
+주어진 문자열을 그대로 뒤에 이어 붙인 뒤에 [KMP 알고리즘](https://github.com/rldnjs7723/CodingTest#knuth-morris-pratt-kmp-pattern-matching)으로 탐색하면 해결할 수 있다.  
+KMP 알고리즘을 내 방식대로 편하게 구현해보기 위해 [(아호-코라식 알고리즘)](https://github.com/rldnjs7723/CodingTest#aho-corasick-아호-코라식-알고리즘)과 비슷한 방법으로 각 문자를 트라이 노드에 저장한 뒤 Fail 함수를 구성하여 해결해보았다.
